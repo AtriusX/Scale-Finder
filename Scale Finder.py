69a6396e84b78904becc28b2cab8fcdfc52fea0e
Atrius <atriusedm@gmail.com>
2018-10-21 06:37:41 -0400

    Added circle of fifths and "minor"/"major" aliases

--- Scale Finder.py
+++ Scale Finder.py
@@ -16,6 +16,7 @@ def wrap(shift, low, high, list):
 			list[i] = list[i] - high - (i == len(list) if 0 else 1)
 		elif (list[i] < low):
 			list[i] = high + list[i] + (i == len(list) if 0 else 1)
+	return list
 
 # Shift the input down to a specific value
 def shift(list, item):
@@ -45,10 +46,29 @@ def find_scale(input):
 		# Return an error if input is incorrect
 		print("Required format: <key> <mode> | <key|mode> all | all")
 
+# Print out the circle of fifths
+def circle_of_fifths(base = "C"):
+	# Make sure the argument is valid
+	if (base not in keys):
+		print("Please provide a valid key for the circle.")
+		return
+
+	print("Keys in the circle of fifths starting at " + base + ": ")
+
+	# Print the circle of fifths for a given key
+	counter = keys.index(base)
+	for _ in range(0, 12):
+		print(keys[counter], end = " ")
+		counter = wrap(7, 0, 11, [counter])[0]
+	print("\n")
+
 # User Input	
 while(True):
 	print("Enter a scale and mode or type 'Exit':", end = " ")
 	text = input()
+
+	if (not text.strip()):
+		continue
 
 	# Exit program
 	if (text.lower() == "exit"):
@@ -56,6 +76,13 @@ while(True):
 	
 	# Display all values for a key
 	split = text.split()
+
+	if (split[1].lower() == "major"):
+		split[1] = "ionian"
+
+	if (split[1].lower() == "minor"):
+		split[1] = "aeolian"
+
 	if (text.lower().endswith("all") and len(split) < 3):
 		
 		# Return all modes for a key
@@ -70,11 +97,19 @@ while(True):
 			for i in keys:
 				find_scale(i + " " + split[0])
 
+	if (split[0].lower() == "circle"):
+		if (len(split) > 1):
+			circle_of_fifths(split[1])
+			continue
+
+		circle_of_fifths()
+
 	# Display every scale
 	if (text.lower() == "all"):
 		for key in keys:
 			for mode in modes:
 				find_scale(key + " " + mode)
 			print("------------------------------------")
-
-	find_scale(text)
+	
+	# Find the scale
+	find_scale(split[0] + " " + split[1])
